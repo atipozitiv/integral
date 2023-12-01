a963f722d54849402914fc04e445ec7ca3f8a1cb
timof <timofejivanov7322@gmail.com>
2023-12-01 20:42:14 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace integral {
+  partial class Form1 {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent() {
+      System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+      System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+      this.label1 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.textBox3 = new System.Windows.Forms.TextBox();
+      this.textBox4 = new System.Windows.Forms.TextBox();
+      this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.действияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.рассчитатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.checkBox1 = new System.Windows.Forms.CheckBox();
+      this.checkBox2 = new System.Windows.Forms.CheckBox();
+      this.checkBox3 = new System.Windows.Forms.CheckBox();
+      this.label2 = new System.Windows.Forms.Label();
+      ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+      this.menuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.label1.Location = new System.Drawing.Point(12, 19);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(224, 164);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Функция:\r\n\r\nНачало интервала:\r\n\r\nКонец интервала:\r\n\r\nТочность:\r\n\r\n";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(103, 21);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(255, 22);
+      this.textBox1.TabIndex = 1;
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(197, 66);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(161, 22);
+      this.textBox2.TabIndex = 2;
+      // 
+      // textBox3
+      // 
+      this.textBox3.Location = new System.Drawing.Point(197, 110);
+      this.textBox3.Name = "textBox3";
+      this.textBox3.Size = new System.Drawing.Size(161, 22);
+      this.textBox3.TabIndex = 3;
+      // 
+      // textBox4
+      // 
+      this.textBox4.Location = new System.Drawing.Point(197, 152);
+      this.textBox4.Name = "textBox4";
+      this.textBox4.Size = new System.Drawing.Size(161, 22);
+      this.textBox4.TabIndex = 4;
+      // 
+      // chart1
+      // 
+      chartArea3.Name = "ChartArea1";
+      this.chart1.ChartAreas.Add(chartArea3);
+      this.chart1.Location = new System.Drawing.Point(448, 21);
+      this.chart1.Name = "chart1";
+      series3.ChartArea = "ChartArea1";
+      series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+      series3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      series3.MarkerSize = 2;
+      series3.Name = "Series1";
+      this.chart1.Series.Add(series3);
+      this.chart1.Size = new System.Drawing.Size(782, 419);
+      this.chart1.TabIndex = 5;
+      this.chart1.Text = "chart1";
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+      this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.действияToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 542);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(1286, 31);
+      this.menuStrip1.TabIndex = 6;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // действияToolStripMenuItem
+      // 
+      this.действияToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.очиститьToolStripMenuItem,
+            this.рассчитатьToolStripMenuItem});
+      this.действияToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.действияToolStripMenuItem.Name = "действияToolStripMenuItem";
+      this.действияToolStripMenuItem.Size = new System.Drawing.Size(98, 27);
+      this.действияToolStripMenuItem.Text = "Действия";
+      // 
+      // очиститьToolStripMenuItem
+      // 
+      this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+      this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(181, 28);
+      this.очиститьToolStripMenuItem.Text = "Очистить";
+      this.очиститьToolStripMenuItem.Click += new System.EventHandler(this.очиститьToolStripMenuItem_Click);
+      // 
+      // рассчитатьToolStripMenuItem
+      // 
+      this.рассчитатьToolStripMenuItem.Name = "рассчитатьToolStripMenuItem";
+      this.рассчитатьToolStripMenuItem.Size = new System.Drawing.Size(181, 28);
+      this.рассчитатьToolStripMenuItem.Text = "Рассчитать";
+      this.рассчитатьToolStripMenuItem.Click += new System.EventHandler(this.рассчитатьToolStripMenuItem_Click);
+      // 
+      // checkBox1
+      // 
+      this.checkBox1.AutoSize = true;
+      this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.checkBox1.Location = new System.Drawing.Point(16, 245);
+      this.checkBox1.Name = "checkBox1";
+      this.checkBox1.Size = new System.Drawing.Size(202, 22);
+      this.checkBox1.TabIndex = 7;
+      this.checkBox1.Text = "метод прямоугольников";
+      this.checkBox1.UseVisualStyleBackColor = true;
+      // 
+      // checkBox2
+      // 
+      this.checkBox2.AutoSize = true;
+      this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.checkBox2.Location = new System.Drawing.Point(16, 273);
+      this.checkBox2.Name = "checkBox2";
+      this.checkBox2.Size = new System.Drawing.Size(141, 22);
+      this.checkBox2.TabIndex = 8;
+      this.checkBox2.Text = "метод трапеций";
+      this.checkBox2.UseVisualStyleBackColor = true;
+      // 
+      // checkBox3
+      // 
+      this.checkBox3.AutoSize = true;
+      this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.checkBox3.Location = new System.Drawing.Point(16, 301);
+      this.checkBox3.Name = "checkBox3";
+      this.checkBox3.Size = new System.Drawing.Size(149, 22);
+      this.checkBox3.TabIndex = 9;
+      this.checkBox3.Text = "метод Симпсона";
+      this.checkBox3.UseVisualStyleBackColor = true;
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.label2.Location = new System.Drawing.Point(16, 372);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(0, 20);
+      this.label2.TabIndex = 10;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1286, 573);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.checkBox3);
+      this.Controls.Add(this.checkBox2);
+      this.Controls.Add(this.checkBox1);
+      this.Controls.Add(this.chart1);
+      this.Controls.Add(this.textBox4);
+      this.Controls.Add(this.textBox3);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.menuStrip1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.TextBox textBox3;
+    private System.Windows.Forms.TextBox textBox4;
+    private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem действияToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem рассчитатьToolStripMenuItem;
+    private System.Windows.Forms.CheckBox checkBox1;
+    private System.Windows.Forms.CheckBox checkBox2;
+    private System.Windows.Forms.CheckBox checkBox3;
+    private System.Windows.Forms.Label label2;
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet.Numerics;
+using org.matheval;
+using org.matheval.Functions;
+
+namespace integral {
+  public partial class Form1 : Form {
+    public Form1() {
+      InitializeComponent();
+      MaximizeBox = false;
+      FormBorderStyle = FormBorderStyle.Fixed3D;
+      this.Text = "Вычисление определенного интеграла";
+    }
+
+    double Func(double x) {
+      Expression func = new Expression(textBox1.Text.ToLower());
+      func.Bind("x", x);
+      double result = func.Eval<double>();
+      return Convert.ToDouble(result);
+    }
+    void DrawChart(double end, double start, string func) {
+      this.chart1.Series[0].Points.Clear();
+      double y;
+      while (start <= end) {
+        y = Func(start);
+        this.chart1.Series[0].Points.AddXY(start, y);
+        start += 0.1;
+      }
+    }
+
+    private void очиститьToolStripMenuItem_Click(object sender, EventArgs e) {
+      textBox1.Text = "";
+      textBox2.Text = "";
+      textBox3.Text = "";
+      textBox4.Text = "";
+      checkBox1.Checked = false;
+      checkBox2.Checked = false;
+      checkBox3.Checked = false;
+      label2.Text = "";
+    }
+
+    void Rectangle(string func, double start, double end, double presision) {
+      double answer = 0;
+      while (start < end) {
+        answer += Math.Abs(presision * Func(start + presision/2));
+        start += presision;
+      }
+      label2.Text += "\nметод прямоугольников: " + Convert.ToString(answer);
+    } 
+    void Trapeze(string func, double start, double end, double presision) {
+      double answer = 0;
+      while (start < end) {
+        answer += Math.Abs((Func(start) + Func(start + presision)) / 2 * presision);
+        start += presision;
+      }
+      label2.Text += "\nметод трапеций: " + Convert.ToString(answer);
+    }
+    void Simpson(string func, double start, double end, double presision) {
+      double Chet = 0;
+      double neChet = 0;
+      double myStart = start + presision;
+      int turn = 2;
+      while (myStart < end) {
+        if (turn % 2 == 0) {
+          Chet += Math.Abs(Func(myStart));
+        } else {
+          neChet += Math.Abs(Func(myStart));
+        }
+        turn += 1;
+        myStart += presision;
+      }
+      double answer = presision / 3 * (Func(start) + 4 * Chet + 2 * neChet + Func(end));
+      label2.Text += "\nметод Симпсона: " + Convert.ToString(answer);
+    }
+
+    private void рассчитатьToolStripMenuItem_Click(object sender, EventArgs e) {
+      try {
+        label2.Text = "";
+        string func = textBox1.Text;
+        double start = Convert.ToDouble(textBox2.Text);
+        double end = Convert.ToDouble(textBox3.Text);
+        double presision = Convert.ToDouble(textBox4.Text);
+        if ((start >= end) || ((end - start) < presision) || (presision <= 0)) {
+          Mistake();
+        } else {
+          DrawChart(end, start, func);
+          if (checkBox1.Checked) Rectangle(func, start, end, presision);
+          if (checkBox2.Checked) Trapeze(func, start, end, presision);
+          if (checkBox3.Checked) Simpson(func, start, end, presision);
+        }
+      } catch {
+        Mistake();
+      }
+    }
+    void Mistake() {
+      label2.Text = "Некорректный ввод";
+    }
+  }
+}
